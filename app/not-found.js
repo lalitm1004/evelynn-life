9206104f1baccbbf87bd513fec833927f7ea4cf9
Lalit M <lalitm1004@gmail.com>
2024-02-11 15:48:11 +0530

    Add button to close tab

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,24 @@
+"use client"
+import { useEffect } from "react";
+
 import { Terminal } from "@/providers/fonts";
 
 export default function Custom404() {
+
+  useEffect(() => {
+
+    const handleClick = () => {
+      window.close();
+    }
+
+    const button = document.getElementById("closepage");
+    button.addEventListener("click", handleClick);
+    return () => button.removeEventListener("click", handleClick);
+  }, []);
+
   return (
     <div className={`${Terminal.className} h-screen flex justify-center items-center`}>
-      <a href="/" target="_self">404 This page does not exist...</a>
+      <button id="closepage">404 This page does not exist...</button>
     </div>
   )
 }
